aca5bcec3e73c30b6f920e7406d188ad388517f9
Ali Faizi <janalifaizi@gmail.com>
2023-11-16 22:16:45 -0500

    Adding the CRUD queries

--- src/com/jdbc/SQLConnection.java
+++ src/com/jdbc/SQLConnection.java
@@ -14,18 +14,32 @@ public class SQLConnection {
 		Connection myConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/xai", "root", "jaf@SQL22!");
 		
 		// We need a CRUD statement 
-		
+	
 		// Statement is for executing our query into the database
 		Statement myStatement = myConnection.createStatement();
 		
 		// Insert Query
 		
-//		String insertQuery = "INSERT INTO xai.student (id, firstName, lastName, age) VALUES ('2', 'Ali', 'Faizi', '30')"; 
+//		String insertQuery = "INSERT INTO xai.student (id, firstName, lastName, age) VALUES ('6', 'Mike', 'JOnes', '32')"; 
 //		
 //		myStatement.execute(insertQuery);
 //		
 //		System.out.println("Records added into the database");
 		
+		
+		// Update Query
+		
+//		String updateQuery = "UPDATE student SET firstName = 'Sarah' WHERE id = 2";
+//		myStatement.execute(updateQuery);
+//		System.out.println("The first name of the student with id number 2 is updated");
+
+		
+		// Delete Query
+		
+//		String deletQuery = "DELETE from student where id = 6";
+//		myStatement.execute(deletQuery);
+//		System.out.println("The record with ID number 6 is deleted.");
+
 		
 		// Select Query
 		
@@ -40,11 +54,10 @@ public class SQLConnection {
 			String myLastName = myResult.getString("lastName");
 			String myAge = myResult.getString("age");
 			
-			System.out.println("Id: " + myId + "First Name: " + myFirstName + "Last Name: " + myLastName + "Age: " + myAge);
-
-				
+			System.out.println("Id: " + myId + " First Name: " + myFirstName + " Last Name: " + myLastName + " Age: " + myAge);	
 			
 		}
+		
 		
 	}
 
